b8f0f848cbf3ef5e027af9bf4d547ecd74781b4c
Phuong <phuwn29@gmail.com>
2024-03-18 14:05:01 +0700

    feat: Add duration to authenticator config

--- src/middleware/tx.go
+++ src/middleware/tx.go
@@ -12,25 +12,26 @@ func AddTransaction(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		tx := server.Get().DB().NewTransaction()
 		c.Set("tx", tx.Tx())
+		next := next(c)
 
 		if rec := recover(); rec != nil {
 			log.Printf("rollback transaction, recover: %v\n", rec)
 			tx.Rollback()
-			return next(c)
+			return next
 		}
 
 		if c.Response().Status == 500 {
 			log.Println("rollback transaction, request failed")
 			tx.Rollback()
-			return next(c)
+			return next
 		}
 
 		if c.Request().Method == "GET" {
 			tx.Rollback()
-			return next(c)
+			return next
 		}
 
 		tx.Commit()
-		return next(c)
+		return next
 	}
 }
